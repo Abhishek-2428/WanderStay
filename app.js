d3e1a0d8f19aba4fc021ec4f8f951c6c97a60e05
abhishek verma <av240704@gmail.com>
2026-07-25 11:58:14 +0530

    add root redirect to /listings

--- app.js
+++ app.js
@@ -97,6 +97,10 @@ app.use("/listings", listingsRouter);
 app.use("/listings/:id/reviews", reviewsRouter);
 app.use("/", userRouter);
 
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
+
 app.all("/{*any}", (req, res, next) => {
   next(new ExpressError(404, "Page Not Found!"));
 });
